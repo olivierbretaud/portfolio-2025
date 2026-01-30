f6173e77045828130a497a3c4c973c01ecea17c3
olivierbretaud <olivierbretaud@hotmail.fr>
2026-01-30 16:45:06 +0100

    feat(add) tepik and ecrf

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -7,27 +7,27 @@ import Image from 'next/image';
 
 export default function Header() {
   const year = new Date().getFullYear() - 2019;
-  const pictures = projects.filter((p:IProject) => p.title === 'Octapharma').map((p:IProject) => p.picture);
+  const pictures = projects.filter((p: IProject) => p.title === 'Bibliocta').map((p: IProject) => p.picture);
 
   return (
     <div className={styles.header}>
       <div className={styles.content}>
         <h1>Olivier Bretaud</h1>
         <h1>Développeur React.js/Node.js</h1>
-        <p>Je développe des applications JavaScript<br/>au sein de l&apos;agence Bakasable depuis {year} ans.</p>
+        <p>Développeur JavaScript full-stack depuis {year} ans,<br />je crée des applications web pensées pour offrir<br />une expérience utilisateur de qualité.</p>
       </div>
       <div className={styles.bg}>
-          <div
-            className={styles.imageContainer}
-          >
-            <Image
-              src={pictures[0]}
-              alt="Project background"
-              fill
-              objectFit="cover"
-              priority={false}
-            />
-          </div>
+        <div
+          className={styles.imageContainer}
+        >
+          <Image
+            src={pictures[0]}
+            alt="Project background"
+            fill
+            objectFit="cover"
+            priority={false}
+          />
+        </div>
       </div>
     </div>
   )
